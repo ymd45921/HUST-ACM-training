975c127555f42161238879521220a1f8d2f199ef
七海 椎名 <ymd45921@163.com>
2019-10-15 20:43:33 +0800

    uncompleted

---
+++ vjudge/problemSet334728/E.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+typedef long long longs;
+
+longs n,l,r;
+int cur;
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin>>n>>l>>r;
+    
+
+}
