10f1b77a49237bea7c63c20b0bc64c647c92528c
Shiroha <WDKRemoteUser@DESKTOP-UC19H9B>
2019-10-20 10:26:42 +0800

    Why not use  STL?

--- vjudge/problemSet336391/E.cpp
+++ vjudge/problemSet336391/E.cpp
@@ -1,55 +1,29 @@
 #include <iostream>
+#include <list>
 
 using namespace std;
 
-int n,len,m;
-int in[10000010];
-bool _next[10000010];
+int n,m;
+int*in;
+list<int> thelist;
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     cin>>n;
-    len = n;
-    for(int i=1,cur=2;i<=n;++i,++cur)
+    in = new int[n];
+    for(int i=0;i<n;++i)
     {
         cin>>in[i];
-        _next[i] = cur;
+        thelist.push_back(in[i]);
     }
-    _next[n-1]=_next[0]=0;
     cin>>m;
-    in[0]=-m;
-    int head = 1;
-    int cur = head;
-    while(_next[cur])
-    if(in[_next[cur]]==m)
-    {
-        _next[cur]=_next[_next[cur]];
-        --len;
-    }
-    else
-    {
-        cur = _next[cur];
-    }
-    
-    if(in[head]==m)
-    {
-        head = _next[head];
-        --len;
-    }
-
-    cout<<n;
-    for(int i=1;i<=n;++i)cout<<in[i]<<' ';
-    cout<<endl;
-    cout<<len;
-    cur = head;
-    while(_next[cur])
-    {
-        cout<<in[cur]<<' ';
-        cur = _next[cur];
-    }
-    cout<<in[cur]<<endl;
+    thelist.remove(m);
+    cout<<n<<endl;
+    for(int i=0;i<n;++i)cout<<in[i]<<' ';
+    cout<<endl<<thelist.size()<<endl;
+    for(auto i=thelist.begin();i!=thelist.end();++i)
+        cout<<*i<<' ';
     return 0;
 }
