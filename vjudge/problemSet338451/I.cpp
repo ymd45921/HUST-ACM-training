106542d065d22bb0eb61578c291be2124dd2dd24
七海 椎名 <ymd45921@163.com>
2019-10-29 09:37:39 +0800

    add introduction

--- vjudge/problemSet338451/I.cpp
+++ vjudge/problemSet338451/I.cpp
@@ -1,64 +1,50 @@
-#include <iostream>
-#include <queue>
-#include <algorithm>
-
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<queue>
+#include<stack>
 using namespace std;
-struct order
-{
-    int q,d;
-    bool operator<(const order&rhs) const           //用于构造大顶堆，想要弹出的是花费时间最大的任务。
-    {
-        return q<rhs.q;
+const int maxn = 800000 + 10;
+struct Node{
+    int s,t;
+    bool operator < (const Node & rhs) const {
+        return s < rhs.s;
     }
-};
-int n,t;
-order in[800080];
-struct cmp
-{
-    bool operator()(order&a, order&b) const         //用于sort排序，优先选择结束时间靠前的项并置前。
-    {
-        return a.d<b.d||(a.d==b.d&&a.q<b.q);
-    }
-};
-
-priority_queue<order> table;
-int cost;
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    cin>>t;
-    while(t--)
-    {
-        while(!table.empty())table.pop();           //清理历史遗留问题
-
-        cin>>n;
-        for(int i=0;i<n;++i)cin>>in[i].q>>in[i].d;
-        sort(in,in+n,cmp());
-
-        cost = 0;
-        for(int i=0;i<n;++i)
-        {
-            if(cost+in[i].q<=in[i].d)
-            {
-                cost+=in[i].q;
-                table.push(in[i]);
-            }
-            else if(!table.empty())
-            {
-                if(table.top().q>in[i].q)
-                {
-                    cost-=table.top().q;
-                    table.pop();
-                    cost+=in[i].q;
-                    table.push(in[i]);
-                }
+}p[maxn];
+priority_queue<Node>q;
+bool cmp(const Node &lhs,const Node & rhs){
+    return lhs.t < rhs.t || (lhs.t == rhs.t && lhs.s < rhs.s);
+}
+int solve(int n){
+    while(!q.empty())q.pop();
+    int cur = 0;
+    for (int i = 0; i < n; ++i){
+        if (cur + p[i].s <= p[i].t){
+            q.push(p[i]);
+            cur += p[i].s;
+        }else {
+            if (q.empty())continue;
+            Node u = q.top();
+            if (u.s > p[i].s){
+                cur = cur - u.s + p[i].s;
+                q.pop();
+                q.push(p[i]);
             }
         }
-        cout<<table.size()<<endl;
+ 
     }
 
+    return q.size();
+}
+int main(){
+
+    int n;
+    scanf("%d",&n);
+    for (int i = 0; i < n ; ++i){
+        scanf("%d%d",&p[i].s,&p[i].t);
+    }
+    sort(p,p+n,cmp);
+    printf("%d\n",solve(n));
+ 
     return 0;
 }
