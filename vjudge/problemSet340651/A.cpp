97a91d1d13e0a46ce8573dac932bc47b133e0cac
七海 椎名 <ymd45921@163.com>
2019-11-05 01:08:48 +0800

    Add problems sources-link

--- vjudge/problemSet340651/A.cpp
+++ vjudge/problemSet340651/A.cpp
@@ -3,81 +3,83 @@
 using namespace std;
 
 bool map[550][550];
-int h,w;
+int h, w;
 char ch;
 int counter = 0;
+int inCounter = 0;
 bool foundFlag = false;
 bool continueFlag = true;
 
 inline bool caster(char ch)
 {
-    if(ch=='*')
-    {
-        ++counter;
-        return true;
-    }
-    else return false;
+	if (ch == '*')
+	{
+		++counter;
+		return true;
+	}
+	else return false;
 }
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
 
-    cin>>h>>w;
-    for(int i=0;i<h;++i)
-        for(int j=0;j<w;++j)
-        {
-            cin>>ch;
-            map[i][j]=caster(ch);
-        }
-    int limh = h-1,limw = w-1;
-    
-    if(counter<5)
-    continueFlag = foundFlag = false;
+	cin >> h >> w;
+	for (int i = 0; i < h; ++i)
+		for (int j = 0; j < w; ++j)
+		{
+			cin >> ch;
+			map[i][j] = caster(ch);
+		}
+	int limh = h - 1, limw = w - 1;
 
-    if(continueFlag)
-    for(int i=1;i<limh;++i)
-    {
-        for(int j=0;j<limw;++j)
-            if(map[i][j])
-            {
-                if(map[i-1][j]&&map[i+1][j]&&map[i][j-1]&&map[i][j+1])
-                {
-                    while(--i>=0&&map[i][j]){map[i][j]=false;}
-                    while(++i<h&&map[i][j]){map[i][j]=false;}
-                    while(--j>=0&&map[i][j]){map[i][j]=false;}
-                    while(++j<w&&map[i][j]){map[i][j]=false;}
-                    foundFlag = true;
-                }
-                else
-                {
-                    foundFlag = false;
-                    continueFlag = false;
-                    break;
-                }
-                
-            }
-        if(!continueFlag)break;   
-    }
-    if(continueFlag&&foundFlag)
-    for(int i=0;i<h;++i)
-        if(map[i][0]||map[i][limw])
-        {
-            foundFlag = false;
-            continueFlag = false;
-            break;
-        }
-    if(continueFlag&&foundFlag)
-    for(int j=0;j<w;++j)
-        if(map[limh][j]||map[0][j])
-        {
-            foundFlag = false;
-            continueFlag = false;
-            break;
-        }
-    if(foundFlag)cout<<"YES";
-    else cout<<"NO";
+	if (counter < 5)
+		continueFlag = foundFlag = false;
 
-    return 0;
+	if (continueFlag)
+		for (int i = 1; i < limh; ++i)
+		{
+			for (int j = 1; j < limw; ++j)
+				if (map[i][j])
+				{
+					if (map[i - 1][j] && map[i + 1][j] && map[i][j - 1] && map[i][j + 1])//isCenter
+					{
+						inCounter = 1;
+						int i_bak = i, j_bak = j;
+						while (--i >= 0 && map[i][j]) { map[i][j] = false; ++inCounter; }
+						i = i_bak;
+						while (++i < h && map[i][j]) { map[i][j] = false; ++inCounter; }
+						i = i_bak;
+						while (--j >= 0 && map[i][j]) { map[i][j] = false; ++inCounter; }
+						j = j_bak;
+						while (++j < w && map[i][j]) { map[i][j] = false; ++inCounter; }
+						i = i_bak; j = j_bak;
+						foundFlag = true;
+						if (inCounter != counter)foundFlag = false;
+						else break;
+					}
+				}
+			if (foundFlag)break;
+		}
+	// if(continueFlag&&foundFlag)
+	// for(int i=0;i<h;++i)
+	//     if(map[i][0]||map[i][limw])
+	//     {
+	//         foundFlag = false;
+	//         continueFlag = false;
+	//         break;
+	//     }
+	// if(continueFlag&&foundFlag)
+	// for(int j=0;j<w;++j)
+	//     if(map[limh][j]||map[0][j])
+	//     {
+	//         foundFlag = false;
+	//         continueFlag = false;
+	//         break;
+	//     }
+	if (foundFlag)cout << "YES";
+	else cout << "NO";
+
+	return 0;
 }
--- vjudge/problemSet340651/MyNote.md
+++ vjudge/problemSet340651/MyNote.md
@@ -4,3 +4,19 @@
 
 ## Problem A @CF1182B  
 查询到十字再去遍历整个图形有点愚蠢，不如统计十字中`'*'`个数：这些`'*'`必须在十字中，否则无解。统计`'*'`可比遍历全图和遍历边界要舒服多了啊！
+
+## Problem B @Uva11624
+
+## Problem C @POJ1190
+
+## Problem D @CF1167C
+
+## Problem E @CF337A
+
+## Problem F @CF687A
+
+## Problem G @CF802J
+
+## Problem H @CF843A
+
+## Problem I @CF862B
