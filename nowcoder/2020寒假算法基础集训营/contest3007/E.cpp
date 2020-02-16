d4f2325467a24873e89f2808636061a2f46c2ffd
七海 椎名 <ymd45921@163.com>
2020-02-16 10:19:51 +0800

    Attempt failed

--- nowcoder/2020寒假算法基础集训营/contest3007/E.cpp
+++ nowcoder/2020寒假算法基础集训营/contest3007/E.cpp
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 #include <iostream>
 #include <cmath>
@@ -11,6 +11,23 @@ typedef long double longd;
 const longs LLMAX = 0x7fffffffffffffffll;
 int t;
 longs n;
+
+bool is[1000100]{0};
+int prime[1000100]{0};
+
+inline void preProcess()
+{
+    int tmp;
+    for(int i=2;i<=1000010;++i)
+    {
+        if(!is[i]) prime[++prime[0]]=i;
+        for(int j=1;j<=prime[0]&&(tmp=i*prime[j])<=1000010;++j)
+        {
+            is[tmp]=1;
+            if(i%prime[j]==0)break;
+        }
+    }
+}
 
 inline longs solve(longs n)
 {
@@ -28,21 +45,29 @@ inline longs solve(longs n)
     if(!(clim&1ll)) clim-=1;
     longs c,c3 = clim*clim*clim;
     if(n%c3);else return clim;
-    for(c=3;c<=clim&&c<8;c+=2)
-        if(n%c) continue;
-        else 
-        {
-            c3 = c*c*c;
-            if(n%c3) return solve(n/c);
-            else return c*solve(n/c3);
-        }
-    for(c=11;c<=clim;c+=2)
-        if(n%c) continue;
+    // for(c=3;c<=clim&&c<8;c+=2)
+    //     if(n%c) continue;
+    //     else
+    //     {
+    //         c3 = c*c*c;
+    //         if(n%c3) return solve(n/c);
+    //         else return c*solve(n/c3);
+    //     }
+    // for(c=11;c<=clim;c+=2)
+    //     if(n%c) continue;
+    //     else
+    //     {
+    //         c3 = c*c*c;
+    //         if(n%c3) return solve(n/c);
+    //         else return c*solve(n/c3);
+    //     }
+    for(int i=1;i<=prime[0]&&prime[i]<=clim;++i)
+        if(n%prime[i]) continue;
         else
         {
-            c3 = c*c*c;
-            if(n%c3) return solve(n/c);
-            else return c*solve(n/c3);
+            c3 = prime[i]*prime[i]*prime[i];
+            if(n%c3) return solve(n/prime[i]);
+            else return prime[i]*solve(n/c3);
         }
     return 1;
 }
@@ -53,6 +78,7 @@ int main()
     cin.tie(nullptr);
 
     cin>>t;
+    preProcess();
     while(t--)
     {
         cin>>n;
