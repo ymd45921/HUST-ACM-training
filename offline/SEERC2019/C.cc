eb7225d78ce1ced4e8e24db38d4a9252583152d4
shiroha <ymd45921@163.com>
2020-10-14 13:48:36 +0800

    不懂

--- offline/SEERC2019/C.cc
+++ offline/SEERC2019/C.cc
@@ -1,6 +1,6 @@
 /**
  *
- *
+ * TODO: 二进制分组是什么……搞不懂（
  */
 #include <bits/stdc++.h>
 
@@ -51,7 +51,15 @@ public:
 } scanner;
 
 const int N = 300, lim = 30;
-int a[N];
+int a[N], b[N];
+#define array(s, n) for (int i = s; i <= n; ++ i) cout << ' ' << a[i]; cout << endl
+
+int diff(int n)
+{
+    auto t = n * (n - 1) / 2, xx = 0, x = 0;
+    while (t --) cin >> x, xx = max(x, xx);
+    return xx;
+}
 
 int main()
 {
@@ -62,7 +70,7 @@ int main()
     freopen("in.txt", "r", stdin);
 #endif
     int n, x; cin >> n;
-    if (n <= lim)
+    if (n < lim)
     {
         for (int i = 1; i <= n; ++ i)
         {
@@ -70,13 +78,71 @@ int main()
             cin >> a[i];
         }
         cout << "3";
-        for (int i = 1; i <= n; ++ i)
-            cout << ' ' << a[i];
-        cout << endl;    
+        array(1, n);
     }
     else
     {
-        
+        cout << "2 " << n;
+        array(1, n);
+        const auto d = diff(n);
+        int l = 2, r = n;
+        while (r > l)
+        {
+            auto mid = l + r >> 1;
+            cout << "2 " << mid;
+            array(1, mid);
+            if (diff(mid) == d) r = mid;
+            else l = mid + 1;
+        }
+        const auto pos = l;
+        unordered_multiset<int> st[20];
+        unordered_set<int> all;
+        for (int i = 1, lg = 0; i <= n; i <<= 1, ++ lg)
+        {
+            vector<int> list;
+            for (int j = 1; j <= n; ++ j)
+                if (!(i & j) || j == pos) continue;
+                else list.push_back(j);
+            if (list.empty()) continue;
+            auto siz = list.size();
+            cout << "2 " << siz + 1 << ' ' << pos;
+            for (auto ii : list) cout << ' ' << ii;
+            cout << endl;
+            auto t = siz * (siz + 1) / 2;
+            while (t --) cin >> x, st[lg].insert(x);
+            if (siz > 1)
+            {
+                cout << "2 " << siz;
+                for (auto ii : list) cout << ' ' << ii;
+                cout << endl;
+                auto t = siz * (siz - 1) / 2;
+                while (t --) cin >> x, st[lg].erase(st[lg].find(x));
+            }   
+            for (auto ii : st[lg]) all.insert(ii);
+        }
+        int lg = 0, npos = -1;
+        while ((1 << lg) <= n) ++ lg;
+        for (auto ii : all)
+        {
+            int t = 0;
+            for (int i = 0; i < lg; ++ i)
+                if (st[lg].count(ii)) t |= (1 << i);
+            b[t] = ii;
+            if (ii == d) npos = t;
+        }
+        cout << "1 " << pos << endl;
+        cin >> a[pos];
+        cout << "1 " << npos << endl;
+        cin >> a[npos];
+        int sgn = a[pos] < a[npos] ? 1 : -1;
+        cout << "3 ";
+        for (int i = 1; i <= n; ++ i)
+        {
+            if (i != pos) a[i] = a[pos] + sgn * b[i];
+            cout << ' ' << a[i];
+        }
+        cout << endl;
+        return 0;
     }
     return 0;
 }
