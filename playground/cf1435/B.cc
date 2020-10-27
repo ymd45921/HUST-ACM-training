634cb1b2cebfc5573024263fbfb6aca4ae95124d
shiroha <ymd45921@163.com>
2020-10-27 17:08:30 +0800

    AB…D fighter

---
+++ playground/cf1435/B.cc
@@ -0,0 +1,104 @@
+/**
+ *
+ *
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+using longs = long long;
+using uint = unsigned;
+using ulongs = unsigned long long;
+using longd = long double;
+
+#define eprintf(x...) fprintf(stderr, x)
+#define var(x) ""#x" = " << x
+#define lll __int128
+#define minimize(a, b) (a = min(a, b))
+#define maximize(a, b) (a = max(a, b))
+
+void print(__int128 x)
+{
+    if (x < 0) { putchar('-'); x = -x; }
+    static char str[40]; int cnt = 0;
+    while (x > 9) { str[cnt ++] = (x % 10) ^ 48; x /= 10;}
+    str[cnt ++] = x ^ 48;
+    while (cnt --) putchar(str[cnt]);
+}
+template <class T>
+void println(T x) {puts(to_string(x).c_str());}
+void println(const char *s) {puts(s);}
+void println(const char ch)
+{putchar(ch), putchar('\n');}
+void println(const lll x)
+{lll xx = x; print(xx), putchar('\n');}
+
+static class Scanner
+{
+    template<class T>
+    inline T read()
+    {
+        T x = 0; int f = 0, ch = getchar();
+        while (!isdigit(ch)) ch == '-' && (f = !f), ch = getchar();
+        while (isdigit(ch)) x = x * 10 + ch - 48, ch = getchar();
+        return f ? -x : x;
+    }
+
+public:
+
+    template <class T>
+    void operator()(T &x){x = read<T>();}
+
+    template <class T, class... Ts>
+    void operator()(T &x, Ts &... y)
+    {(*this)(x), (*this)(y...);}
+
+    void operator()(char *x){scanf("%s", x);}
+    void operator()(char &x){x = (char)getchar();}
+
+    int nextInt() {return read<int>();}
+    longs nextLongs() {return read<longs>();}
+    lll nextInt128() {return read<lll>();}
+    char nextChar() {return (char)getchar();}
+} scanner;
+
+const int N = 520;
+int row[N][N], col[N][N];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+#if 0
+    freopen("in.txt", "r", stdin);
+#endif
+    Scanner sc;
+    int t = sc.nextInt();
+    array<int*, N * N> ans{};
+    while (t --)
+    {
+        int n = sc.nextInt(), m = sc.nextInt();
+        for (int i = 1; i <= n; ++ i)
+            for (int j = 1; j <= m; ++ j)
+                row[i][j] = sc.nextInt();
+        for (int j = 1; j <= m; ++ j)
+            for (int i = 1; i <= n; ++ i)
+                col[j][i] = sc.nextInt();
+        int top = row[1][1], *list;
+        for (int j = 1; j <= m; ++ j)
+            for (int i = 1; i <= n; ++ i)
+                if (top == col[j][i])
+                {list = col[j]; break;}
+        for (int i = 1; i <= n; ++ i)
+            ans[row[i][1]] = row[i];
+        for (int i = 1; i <= n; ++ i)
+        {
+            for (int j = 1; j < m; ++ j)
+                printf("%d ", ans[list[i]][j]);
+            printf("%d\n", ans[list[i]][m]);
+        }
+    }
+
+    return 0;
+}
+
