32a413ceb406ff38f6ba2529477275555fcaeeb9
nanami <ymd45921@163.com>
2020-11-03 00:36:23 +0800

    try to complete 680

---
+++ playground/cf1445/E.cc
@@ -0,0 +1,100 @@
+/**
+ *
+ * 题面的条件：
+ * + 一共有 n 个学生，每个学生属于第 [1, k] 组
+ * + 两位学生之间可能有连边，有 m 条这样的连边
+ * + 希望选择两个组的学生，可以重新分配为两队，队间不包含连边
+ * + 问对于给定的图，选出这样两组学生的方案数量
+ * 
+ * 考场上预读：
+ * + 简单无向图，每个学生有一种颜色，不超过 k 种颜色
+ * + 对于选定的两组学生 I, J，包含的边数不超过 #(I + J) - 1 则成立
+ * + 想要预处理一些信息，从而快速计算出所有对的有效性
+ * 
+ * 标准做法：
+ * 
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+using longs = long long;
+using uint = unsigned;
+using ulongs = unsigned long long;
+using longd = long double;
+using lll = __int128;
+
+#define minimize(a, b) ((a) = min(a, b))
+#define maximize(a, b) ((a) = max(a, b))
+#define sgn(x) ((x) < 0 ? -1 : (x) > 0)
+#define puti(n) puts(to_string(n).c_str())
+
+#if 1
+#define eprintf(x...) fprintf(stderr, x)
+#define var(x) ""#x" = " << x
+#define watch(args...) cerr << args << endl
+#define $$ << ", " <<
+#define vars(x, y...) var(x) << ", " << vars(y)
+#else
+#define eprintf(...)
+#define watch(...)
+#endif
+
+template <class T>
+void print(T x)
+{
+    if (x < 0) { putchar('-'); x = -x; }
+    static char str[40]; int cnt = 0;
+    while (x > 9) { str[cnt ++] = (x % 10) + 48; x /= 10;}
+    str[cnt ++] = x + 48;
+    while (cnt --) putchar(str[cnt]);
+}
+void print(char *s) {printf(s);}
+void print(char ch) {putchar(ch);}
+template <class T>
+void println(T x)
+{print(x), putchar('\n');}
+
+static class Scanner
+{
+    template<class T>
+    inline T read()
+    {
+        T x = 0; int f = 0, ch = getchar();
+        while (!isdigit(ch)) ch == '-' && (f = !f), ch = getchar();
+        while (isdigit(ch)) x = x * 10 + ch - 48, ch = getchar();
+        return f ? -x : x;
+    }
+
+    static inline bool isBlank(int x)
+    {return x == ' ' || x == '\n';}
+public:
+
+    template <class T>
+    void operator()(T &x){x = read<T>();}
+
+    template <class T, class... Ts>
+    void operator()(T &x, Ts &... y)
+    {(*this)(x), (*this)(y...);}
+
+    void operator()(char *x){scanf("%s", x);}
+    void operator()(char &x){do x = (char)getchar(); while (isBlank(x));}
+
+    int nextInt() {return read<int>();}
+    longs nextLongs() {return read<longs>();}
+    lll nextInt128() {return read<lll>();}
+    char nextChar() {char x; (*this)(x); return x;}
+} scanner;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+#if 0
+    freopen("in.txt", "r", stdin);
+#endif
+
+
+    return 0;
+}
+
