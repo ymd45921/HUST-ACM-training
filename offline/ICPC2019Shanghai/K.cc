7a3b3b5401fddd9a01d4a717b33d90fb8ca80cf2
shiroha <ymd45921@163.com>
2020-12-12 20:40:21 +0800

    爬行比赛，去年上海站这真是树穴竞赛（

---
+++ offline/ICPC2019Shanghai/K.cc
@@ -0,0 +1,174 @@
+/**
+ *
+ *
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+using longs = long long;
+using uint = unsigned;
+using ulongs = unsigned long long;
+using longd = long double;
+using lll = __int128;
+
+#define minimize(a, b...) ((a) = min({(a), b}))
+#define maximize(a, b...) ((a) = max({(a), b}))
+#define sgn(x) ((x) < 0 ? -1 : (x) > 0)
+#define puti(n) puts(to_string(n).c_str())
+
+#if 1
+#define eprintf(x...) fprintf(stderr, x)
+#define eputs(str) fputs(str, stderr), fputc('\n', stderr)
+#define var(x) ""#x" = " << x
+#define watch(...) trace(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define eprintf(...)
+#define eputs(...)
+#define watch(...)
+#endif
+
+template <class printable>
+void trace(const char *name, printable &&value)
+{cerr << name << " = " << value << endl;}
+template <class printable, class ...args>
+void trace(const char *names, printable &&value, args &&...list)
+{
+    const char *separate = strchr(names + 1, ',');
+    cerr.write(names, separate - names) << " = " << value << ',';
+    trace(separate + 1, list...);
+}
+
+template <class T>
+void print(T x)
+{
+    if (x < 0) { putchar('-'); x = -x; }
+    static char str[40]; int cnt = 0;
+    while (x > 9) { str[cnt ++] = (x % 10) + 48; x /= 10;}
+    str[cnt ++] = x + 48;
+    while (cnt --) putchar(str[cnt]);
+}
+void print(const char *s) {fputs(s, stdout);}
+void print(char *s) {fputs(s, stdout);}
+void print(string &s) {print(s.c_str());}
+void print(char ch) {putchar(ch);}
+template <class T, class ...Ts>
+void print(T x, Ts ...xs) {print(x), print(xs...);}
+void println() {puts("");}
+template <class T>
+void println(T x)
+{print(x), putchar('\n');}
+
+static class Scanner
+{
+    template<class T>
+    inline T read()
+    {
+        T x = 0; int f = 0, ch = getchar();
+        while (!isdigit(ch)) ch == '-' && (f = !f), ch = getchar();
+        while (isdigit(ch)) x = x * 10 + ch - 48, ch = getchar();
+        return f ? -x : x;
+    }
+
+    static inline bool isBlank(int x)
+    {return x == ' ' || x == '\n';}
+public:
+
+    template <class T>
+    void operator()(T &x){x = read<T>();}
+
+    template <class T, class... Ts>
+    void operator()(T &x, Ts &... y)
+    {(*this)(x), (*this)(y...);}
+    template <class T, class... Ts>
+    void operator()(T &&x, Ts &&... y)
+    {(*this)(x), (*this)(y...);}
+
+    void operator()(char *x){scanf("%s", x);}
+    void operator()(char &x){do x = (char)getchar(); while (isBlank(x));}
+
+    int nextInt() {return read<int>();}
+    longs nextLongs() {return read<longs>();}
+    lll nextInt128() {return read<lll>();}
+    char nextChar() {char x; (*this)(x); return x;}
+} scanner;
+
+const int N = 20, M = N * N;
+const int inf = 0x3f3f3f3f;
+
+struct edge
+{
+    int u, v, w, next;
+    edge() = default;
+    edge(int u, int v, int w, int next)
+            : u(u), v(v), w(w), next(next) {}
+};
+
+namespace FWS
+{
+    int head[N], in[N], out[N];
+    int tot;
+    edge ee[M * 2];
+
+    void init(int n = N - 1)
+    {
+        memset(head, -1, sizeof(int) * (n + 1));
+        memset(in, 0, sizeof(int) * (n + 1));
+        memset(out, 0, sizeof(int) * (n + 1));
+        tot = 0;
+    }
+
+    void addEdge(int u, int v, int w = 1)
+    {
+        ee[tot] = edge(u, v, w, head[u]);
+        head[u] = tot ++;
+        ++ out[u], ++ in[v];
+    }
+
+    template <class fun>
+    void forEach(int u, const fun process)
+    {
+        for (auto c = head[u];
+             c != -1;
+             c = ee[c].next)
+            process(ee[c]);
+    }
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+#if 0
+    freopen("in.txt", "r", stdin);
+#endif
+    int t = scanner.nextInt(), Case = 0;
+    while (t --)
+    {
+        uint n, m, u, v;
+        scanner(n, m);
+        FWS::init(n);
+        while (m --)
+        {
+            scanner(u, v);
+            FWS::addEdge(u, v);
+            FWS::addEdge(v, u);
+        }
+        uint lim = (1u << n) - 1, ans = 0;
+        for (uint i = 1; i < lim; ++ i)
+        {
+            uint bits = i << 1u, tmp = 0;
+            for (uint j = 1; j <= n; ++ j)
+                if (bits & (1u << j))
+                    FWS::forEach(j, [&](const edge &e)
+                    {
+                        uint v = e.v;
+                        if (!(bits & (1u << v))) ++ tmp;
+                    });
+            maximize(ans, tmp);
+        }
+        printf("Case #%d: ", ++ Case);
+        println(ans);
+    }
+    return 0;
+}
