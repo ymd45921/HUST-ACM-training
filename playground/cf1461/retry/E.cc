ee66589df325ea44c3e7710f68689beaa309b8ff
shiroha <ymd45921@163.com>
2020-12-12 21:58:17 +0800

    div 2 的 E 也就这啊（

---
+++ playground/cf1461/retry/E.cc
@@ -0,0 +1,153 @@
+/**
+ *
+ * 这个 E 也不算很难啊……
+ * 我日，这把岂不是血亏
+ *
+ * 这个 x 众人皆醉的范围还是有点东西的（
+ * FPT: 尝试加水一定在扣水前才算是完全模拟
+ * TLE: 跳跃应当一次扣到要加水，不然太慢了
+ * WA84: x > y 搞反了，应该是先加后减==
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+using longs = long long;
+using uint = unsigned;
+using ulongs = unsigned long long;
+using longd = long double;
+using lll = __int128;
+
+#define minimize(a, b...) ((a) = min({(a), b}))
+#define maximize(a, b...) ((a) = max({(a), b}))
+#define sgn(x) ((x) < 0 ? -1 : (x) > 0)
+#define puti(n) puts(to_string(n).c_str())
+
+#if 1
+#define eprintf(x...) fprintf(stderr, x)
+#define eputs(str) fputs(str, stderr), fputc('\n', stderr)
+#define var(x) ""#x" = " << x
+#define watch(...) trace(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define eprintf(...)
+#define eputs(...)
+#define watch(...)
+#endif
+
+template <class printable>
+void trace(const char *name, printable &&value)
+{cerr << name << " = " << value << endl;}
+template <class printable, class ...args>
+void trace(const char *names, printable &&value, args &&...list)
+{
+    const char *separate = strchr(names + 1, ',');
+    cerr.write(names, separate - names) << " = " << value << ',';
+    trace(separate + 1, list...);
+}
+
+template <class T>
+void print(T x)
+{
+    if (x < 0) { putchar('-'); x = -x; }
+    static char str[40]; int cnt = 0;
+    while (x > 9) { str[cnt ++] = (x % 10) + 48; x /= 10;}
+    str[cnt ++] = x + 48;
+    while (cnt --) putchar(str[cnt]);
+}
+void print(const char *s) {fputs(s, stdout);}
+void print(char *s) {fputs(s, stdout);}
+void print(string &s) {print(s.c_str());}
+void print(char ch) {putchar(ch);}
+template <class T, class ...Ts>
+void print(T x, Ts ...xs) {print(x), print(xs...);}
+void println() {puts("");}
+template <class T>
+void println(T x)
+{print(x), putchar('\n');}
+template <class T, class ...Ts>
+void println(T x, Ts ...xs)
+{print(x), putchar(' '), println(xs...);}
+
+static class Scanner
+{
+    template<class T>
+    inline T read()
+    {
+        T x = 0; int f = 0, ch = getchar();
+        while (!isdigit(ch)) ch == '-' && (f = !f), ch = getchar();
+        while (isdigit(ch)) x = x * 10 + ch - 48, ch = getchar();
+        return f ? -x : x;
+    }
+
+    static inline bool isBlank(int x)
+    {return x == ' ' || x == '\n';}
+public:
+
+    template <class T>
+    void operator()(T &x){x = read<T>();}
+
+    template <class T, class... Ts>
+    void operator()(T &x, Ts &... y)
+    {(*this)(x), (*this)(y...);}
+    template <class T, class... Ts>
+    void operator()(T &&x, Ts &&... y)
+    {(*this)(x), (*this)(y...);}
+
+    void operator()(char *x){scanf("%s", x);}
+    void operator()(char &x){do x = (char)getchar(); while (isBlank(x));}
+
+    int nextInt() {return read<int>();}
+    longs nextLongs() {return read<longs>();}
+    lll nextInt128() {return read<lll>();}
+    char nextChar() {char x; (*this)(x); return x;}
+} scanner;
+
+const int N = 1e6 + 5;
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+#if 0
+    freopen("in.txt", "r", stdin);
+#endif
+    longs k, l, r, t, x, y;
+    scanner(k, l, r, t, x, y);
+    if (y < x)
+    {
+        if (k + y <= r) k += y, -- t;
+        auto lost = x - y, rest = k - l;
+        auto days = (rest - x) / lost + 1;
+        if (days >= t) println("Yes");
+        else println("No");
+    }
+    else if (y > x) // simulate the recursion
+    {
+        auto now = k, low = k - l, high = r - k, res = k % x;
+        const auto update = [&]
+        {low = now - l, high = r - now, res = now % x;};
+        const auto validate = [&]
+        {return now >= l && now <= r;};
+        bitset<N> reached;
+        bool legal = true;
+        while (t)
+        {
+            if (reached[res]) break;
+            else reached[res] = true;
+            auto step = min(t, low / x);
+            if (t <= step) break;
+            else t -= step, now -= step * x;
+            if (now + y <= r) now += y;
+            now -= x, -- t, update();
+            if (!validate()) {legal = false; break;}
+        }
+        println(legal ? "Yes" : "No");
+    }
+    else
+    {
+        auto low = k - l, high = r - k;
+        if (low < x && high < y) println("No");
+        else println("Yes");
+    }
+    return 0;
+}
