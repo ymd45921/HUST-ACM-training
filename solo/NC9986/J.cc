d6a4c60972c1c5c145198bc62910bbda3aa29507
nanami <ymd45921@163.com>
2021-02-24 15:15:03 +0800

    每日一套：轻松愉快娱乐场，被自己坑死（

---
+++ solo/NC9986/J.cc
@@ -0,0 +1,162 @@
+/**
+ *
+ * 因为修改遗留代码带来的问题
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+using longs = long long;
+using uint = unsigned;
+using ulongs = unsigned long long;
+using longd = long double;
+using lll = __int128;
+
+#define minimize(a, b...) ((a) = min({(a), b}))
+#define maximize(a, b...) ((a) = max({(a), b}))
+#define sgn(x) ((x) < 0 ? -1 : (x) > 0)
+#define puti(n) puts(to_string(n).c_str())
+
+#if 1
+#define eprintf(x...) fprintf(stderr, x)
+#define eputs(str) fputs(str, stderr), fputc('\n', stderr)
+#define var(x) ""#x" = " << x
+#define watch(...) trace(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define eprintf(...)
+#define eputs(...)
+#define watch(...)
+#endif
+
+template <class printable>
+void trace(const char *name, printable &&value)
+{cerr << name << " = " << value << endl;}
+template <class printable, class ...args>
+void trace(const char *names, printable &&value, args &&...list)
+{
+    const char *separate = strchr(names + 1, ',');
+    cerr.write(names, separate - names) << " = " << value << ',';
+    trace(separate + 1, list...);
+}
+
+template <class T>
+void print(T x)
+{
+    if (x < 0) { putchar('-'); x = -x; }
+    static char str[40]; int cnt = 0;
+    while (x > 9) { str[cnt ++] = (x % 10) + 48; x /= 10;}
+    str[cnt ++] = x + 48;
+    while (cnt --) putchar(str[cnt]);
+}
+void print(string &s) {printf(s.c_str());}
+void print(const char *s) {printf(s);}
+void print(char *s) {printf(s);}
+void print(char ch) {putchar(ch);}
+template <class T, class ...Ts>
+void print(T x, Ts ...xs) {print(x), print(xs...);}
+void println() {puts("");}
+template <class T>
+void println(T x)
+{print(x), putchar('\n');}
+
+static class Scanner
+{
+    template<class T>
+    inline T read()
+    {
+        T x = 0; int f = 0, ch = getchar();
+        while (!isdigit(ch)) ch == '-' && (f = !f), ch = getchar();
+        while (isdigit(ch)) x = x * 10 + ch - 48, ch = getchar();
+        return f ? -x : x;
+    }
+
+    static inline bool isBlank(int x)
+    {return x == ' ' || x == '\n';}
+public:
+
+    template <class T>
+    void operator()(T &x){x = read<T>();}
+
+    template <class T, class... Ts>
+    void operator()(T &x, Ts &... y)
+    {(*this)(x), (*this)(y...);}
+    template <class T, class... Ts>
+    void operator()(T &&x, Ts &&... y)
+    {(*this)(x), (*this)(y...);}
+
+    void operator()(char *x){scanf("%s", x);}
+    void operator()(char &x){do x = (char)getchar(); while (isBlank(x));}
+
+    int nextInt() {return read<int>();}
+    longs nextLongs() {return read<longs>();}
+    lll nextInt128() {return read<lll>();}
+    char nextChar() {char x; (*this)(x); return x;}
+} scanner;
+
+const int N = 5050, M = N * N;
+const int inf = 0x3f3f3f3f;
+
+namespace DSU
+{
+    int fa[N], siz[N];
+
+    void init(int n)
+    {
+        for (int i = 0; i <= n; ++ i)
+            fa[i] = i, siz[i] = 1;
+    }
+
+    int father(int u)
+    {return fa[u] == u ? u : (fa[u] = father(fa[u]));}
+
+    void join(int x, int y)
+    {
+        int fx = father(x), fy = father(y);
+        if (fx == fy) return;
+        if (siz[fx] <= siz[fy]) fa[fx] = fy, siz[fy] += siz[fx];
+        else fa[fy] = fx, siz[fx] += siz[fy];
+    }
+
+    int size(int u) {return siz[father(u)];}
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+#if 0
+    freopen("in.txt", "r", stdin);
+#endif
+    unordered_map<string, int> reflect;
+    vector<pair<int, pair<int, int>>> edges;
+    int n, q, val, st, u, v;
+    string tmp, a, b;
+    while (cin >> n >> q >> tmp)
+    {
+        int tot = 0;
+        reflect.clear(), edges.clear();
+        if (!reflect.count(tmp)) reflect[tmp] = ++ tot;
+        st = reflect[tmp];
+        DSU::init(n + 1);
+        bitset<N> city;
+        while (q --)
+        {
+            cin >> a >> b >> val;
+            if (!reflect.count(a)) reflect[a] = ++ tot;
+            if (!reflect.count(b)) reflect[b] = ++ tot;
+            u = reflect[a], v = reflect[b];
+            edges.emplace_back(val, make_pair(u, v));
+            city[u] = city[v] = true;
+        }
+        sort(edges.begin(), edges.end());
+        lll ans = 0;
+        using DSU::father, DSU::join;
+        for (auto [w, e] : edges)
+            if (father(e.first) != father(e.second))
+                join(e.first, e.second), ans += w;
+        join(st, 0);
+        bool ok = DSU::size(0) == n + 1;
+        if (ok) println(ans); else puts("No!");
+    }
+    return 0;
+}
