aae0535b3f24aa8a9e9cba265a56ccfea0eaa0cf
shiroha <ymd45921@163.com>
2021-03-03 21:40:13 +0800

    每日一题：这个七海就是逊啦

---
+++ playground/2021-Spr/cf1491/retry/D.cc
@@ -0,0 +1,127 @@
+/**
+ *
+ * 还是没有想清楚：
+ * - 在任何情况（后缀）下都应该满足
+ * - u 的位数多于 v 的，采用可能把 u 挪成 v
+ * - 位数相等的情况下，必须保证 v 的每一位右侧 u 都有 
+ * - v >= u 保证了当位数不等时，v 的整体位数高于 u
+ * - 即使 v 有部分位比较弱势，v 一定有高位补偿这一点
+ * - 因此，只需要将这之间的位数合并到高位位置即可
+ * 综上所述，也没有必要预处理这个预处理那个的
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+using longs = long long;
+using uint = unsigned;
+using ulongs = unsigned long long;
+using longd = long double;
+using lll = __int128;
+
+#define minimize(a, b...) ((a) = min({(a), b}))
+#define maximize(a, b...) ((a) = max({(a), b}))
+#define sgn(x) ((x) < 0 ? -1 : (x) > 0)
+#define puti(n) puts(to_string(n).c_str())
+
+#if 1
+#define eprintf(x...) fprintf(stderr, x)
+#define eputs(str) fputs(str, stderr), fputc('\n', stderr)
+#define var(x) ""#x" = " << x
+#define watch(...) trace(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define eprintf(...)
+#define eputs(...)
+#define watch(...)
+#endif
+
+template <class printable>
+void trace(const char *name, printable &&value)
+{cerr << name << " = " << value << endl;}
+template <class printable, class ...args>
+void trace(const char *names, printable &&value, args &&...list)
+{
+    const char *separate = strchr(names + 1, ',');
+    cerr.write(names, separate - names) << " = " << value << ',';
+    trace(separate + 1, list...);
+}
+
+template <class T>
+void print(T x)
+{
+    if (x < 0) { putchar('-'); x = -x; }
+    static char str[40]; int cnt = 0;
+    while (x > 9) { str[cnt ++] = (x % 10) + 48; x /= 10;}
+    str[cnt ++] = x + 48;
+    while (cnt --) putchar(str[cnt]);
+}
+void print(const char *s) {fputs(s, stdout);}
+void print(char *s) {fputs(s, stdout);}
+void print(string &s) {print(s.c_str());}
+void print(char ch) {putchar(ch);}
+template <class T, class ...Ts>
+void print(T x, Ts ...xs) {print(x), print(xs...);}
+void println() {puts("");}
+template <class T>
+void println(T x)
+{print(x), putchar('\n');}
+
+static class Scanner
+{
+    template<class T>
+    inline T read()
+    {
+        T x = 0; int f = 0, ch = getchar();
+        while (!isdigit(ch)) ch == '-' && (f = !f), ch = getchar();
+        while (isdigit(ch)) x = x * 10 + ch - 48, ch = getchar();
+        return f ? -x : x;
+    }
+
+    static inline bool isBlank(int x)
+    {return x == ' ' || x == '\n';}
+public:
+
+    template <class T>
+    void operator()(T &x){x = read<T>();}
+
+    template <class T, class... Ts>
+    void operator()(T &x, Ts &... y)
+    {(*this)(x), (*this)(y...);}
+    template <class T, class... Ts>
+    void operator()(T &&x, Ts &&... y)
+    {(*this)(x), (*this)(y...);}
+
+    void operator()(char *x){scanf("%s", x);}
+    void operator()(char &x){do x = (char)getchar(); while (isBlank(x));}
+
+    int nextInt() {return read<int>();}
+    longs nextLongs() {return read<longs>();}
+    lll nextInt128() {return read<lll>();}
+    char nextChar() {char x; (*this)(x); return x;}
+} scanner;
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+#if 0
+    freopen("in.txt", "r", stdin);
+#endif
+    int T = scanner.nextInt();
+    ulongs x, y;
+    while (T --)
+    {
+        scanner(x, y);
+        bitset<64> xx = x, yy = y;
+        bool ok = x <= y; 
+        int balance = 0;
+        for (int i = 0; i < 32; ++ i)
+        {
+            balance += xx[i],
+            balance -= yy[i];
+            ok &= balance >= 0;
+        }
+        println(ok ? "YES" : "NO");
+    }
+    return 0;
+}
