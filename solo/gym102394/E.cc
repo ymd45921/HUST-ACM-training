5add6e4af406df022c4ab09777503b3e6b29c348
shiraha <ymd45921@163.com>
2021-03-04 15:37:28 +0800

    每日一套：菜死了

---
+++ solo/gym102394/E.cc
@@ -0,0 +1,140 @@
+/**
+ *
+ * 暴力合并不可取
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+using longs = long long;
+using uint = unsigned;
+using ulongs = unsigned long long;
+using longd = long double;
+using lll = __int128;
+
+#define minimize(a, b...) ((a) = min({(a), b}))
+#define maximize(a, b...) ((a) = max({(a), b}))
+#define sgn(x) ((x) < 0 ? -1 : (x) > 0)
+#define puti(n) puts(to_string(n).c_str())
+
+#if 1
+#define eprintf(x...) fprintf(stderr, x)
+#define eputs(str) fputs(str, stderr), fputc('\n', stderr)
+#define var(x) ""#x" = " << x
+#define watch(...) trace(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define eprintf(...)
+#define eputs(...)
+#define watch(...)
+#endif
+
+template <class printable>
+void trace(const char *name, printable &&value)
+{cerr << name << " = " << value << endl;}
+template <class printable, class ...args>
+void trace(const char *names, printable &&value, args &&...list)
+{
+    const char *separate = strchr(names + 1, ',');
+    cerr.write(names, separate - names) << " = " << value << ',';
+    trace(separate + 1, list...);
+}
+
+template <class T>
+void print(T x)
+{
+    if (x < 0) { putchar('-'); x = -x; }
+    static char str[40]; int cnt = 0;
+    while (x > 9) { str[cnt ++] = (x % 10) + 48; x /= 10;}
+    str[cnt ++] = x + 48;
+    while (cnt --) putchar(str[cnt]);
+}
+void print(const char *s) {fputs(s, stdout);}
+void print(char *s) {fputs(s, stdout);}
+void print(string &s) {print(s.c_str());}
+void print(char ch) {putchar(ch);}
+template <class T, class ...Ts>
+void print(T x, Ts ...xs) {print(x), print(xs...);}
+void println() {puts("");}
+template <class T>
+void println(T x)
+{print(x), putchar('\n');}
+
+static class Scanner
+{
+    template<class T>
+    inline T read()
+    {
+        T x = 0; int f = 0, ch = getchar();
+        while (!isdigit(ch)) ch == '-' && (f = !f), ch = getchar();
+        while (isdigit(ch)) x = x * 10 + ch - 48, ch = getchar();
+        return f ? -x : x;
+    }
+
+    static inline bool isBlank(int x)
+    {return x == ' ' || x == '\n';}
+public:
+
+    template <class T>
+    void operator()(T &x){x = read<T>();}
+
+    template <class T, class... Ts>
+    void operator()(T &x, Ts &... y)
+    {(*this)(x), (*this)(y...);}
+    template <class T, class... Ts>
+    void operator()(T &&x, Ts &&... y)
+    {(*this)(x), (*this)(y...);}
+
+    void operator()(char *x){scanf("%s", x);}
+    void operator()(char &x){do x = (char)getchar(); while (isBlank(x));}
+
+    int nextInt() {return read<int>();}
+    longs nextLongs() {return read<longs>();}
+    lll nextInt128() {return read<lll>();}
+    char nextChar() {char x; (*this)(x); return x;}
+} scanner;
+
+const int N = 1e6 + 5;
+unordered_map<int, longs> cnt[N], po[N];
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+#if 0
+    freopen("in.txt", "r", stdin);
+#endif
+    int T = scanner.nextInt();
+    while (T --)
+    {
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; ++ i)
+        {
+            int op = scanner.nextInt();
+            cnt[i].clear(), po[i].clear();
+            if (op == 1)
+            {
+                int k = scanner.nextInt(), x;
+                for (int j = 1; j <= k; ++ j)
+                    x = scanner.nextInt(),
+                    ++ cnt[i][x];
+                po[i][i] = 1;    
+            }
+            else 
+            {
+                int x = scanner.nextInt(),
+                    y = scanner.nextInt();
+                for (auto [k, v] : po[x]) po[i][k] += v;
+                for (auto [k, v] : po[y]) po[i][k] += v;    
+            }
+        }
+        if (cnt[n].empty())
+            for (auto [k, v] : po[n])
+                for (auto [i, t] : cnt[k])
+                    cnt[n][i] += t * v;
+        longs max_v = 0, sum = 0;            
+        for (auto [k, v] : cnt[n])   
+            maximize(max_v, v), sum += v;
+        println(min(sum, (sum - max_v) * 2));             
+    }
+    return 0;
+}
