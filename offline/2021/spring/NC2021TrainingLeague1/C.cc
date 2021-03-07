5b04d5cd966ca6b6a2d19c2519b377197f8b3f7c
shiraha <ymd45921@163.com>
2021-03-07 16:46:08 +0800

    每日一套：大力白给

---
+++ offline/2021/spring/NC2021TrainingLeague1/C.cc
@@ -0,0 +1,179 @@
+/**
+ *
+ * 这个样例的 11112，，我感觉我甚至在这件训练室做过这个题
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+using longs = long long;
+using uint = unsigned;
+using ulongs = unsigned long long;
+using longd = long double;
+using lll = __int128;
+
+#define minimize(a, b...) ((a) = min({(a), b}))
+#define maximize(a, b...) ((a) = max({(a), b}))
+#define sgn(x) ((x) < 0 ? -1 : (x) > 0)
+#define puti(n) puts(to_string(n).c_str())
+
+#if 1
+#define eprintf(x...) fprintf(stderr, x)
+#define eputs(str) fputs(str, stderr), fputc('\n', stderr)
+#define var(x) ""#x" = " << x
+#define watch(...) trace(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define eprintf(...)
+#define eputs(...)
+#define watch(...)
+#endif
+
+template <class printable>
+void trace(const char *name, printable &&value)
+{cerr << name << " = " << value << endl;}
+template <class printable, class ...args>
+void trace(const char *names, printable &&value, args &&...list)
+{
+    const char *separate = strchr(names + 1, ',');
+    cerr.write(names, separate - names) << " = " << value << ',';
+    trace(separate + 1, list...);
+}
+
+template <class T>
+void print(T x)
+{
+    if (x < 0) { putchar('-'); x = -x; }
+    static char str[40]; int cnt = 0;
+    while (x > 9) { str[cnt ++] = (x % 10) + 48; x /= 10;}
+    str[cnt ++] = x + 48;
+    while (cnt --) putchar(str[cnt]);
+}
+void print(const char *s) {fputs(s, stdout);}
+void print(char *s) {fputs(s, stdout);}
+void print(string &s) {print(s.c_str());}
+void print(char ch) {putchar(ch);}
+template <class T, class ...Ts>
+void print(T x, Ts ...xs) {print(x), print(xs...);}
+void println() {puts("");}
+template <class T>
+void println(T x)
+{print(x), putchar('\n');}
+
+static class Scanner
+{
+    template<class T>
+    inline T read()
+    {
+        T x = 0; int f = 0, ch = getchar();
+        while (!isdigit(ch)) ch == '-' && (f = !f), ch = getchar();
+        while (isdigit(ch)) x = x * 10 + ch - 48, ch = getchar();
+        return f ? -x : x;
+    }
+
+    static inline bool isBlank(int x)
+    {return x == ' ' || x == '\n';}
+public:
+
+    template <class T>
+    void operator()(T &x){x = read<T>();}
+
+    template <class T, class... Ts>
+    void operator()(T &x, Ts &... y)
+    {(*this)(x), (*this)(y...);}
+    template <class T, class... Ts>
+    void operator()(T &&x, Ts &&... y)
+    {(*this)(x), (*this)(y...);}
+
+    void operator()(char *x){scanf("%s", x);}
+    void operator()(char &x){do x = (char)getchar(); while (isBlank(x));}
+
+    int nextInt() {return read<int>();}
+    longs nextLongs() {return read<longs>();}
+    lll nextInt128() {return read<lll>();}
+    char nextChar() {char x; (*this)(x); return x;}
+} scanner;
+
+struct fast_read
+{
+    template <class name>
+    fast_read operator, (name &x)
+    {scanner(x); return fast_read{};}
+} fastRead;
+
+#define $$ fastRead,
+#define int$(...) int __VA_ARGS__; $$ __VA_ARGS__
+#define i64$(...) longs __VA_ARGS__; $$ __VA_ARGS__
+#define lll$(...) lll __VA_ARGS__; $$ __VA_ARGS__
+
+int mul[10][10];
+bitset<10> prod[10];
+int ans[30], test[30];
+char s[30];
+longs a = 0x3f3f3f3f3f3f3f3f;
+
+bool check(int len)
+{
+    memset(test, 0, sizeof test);
+    for (int i = 0; i < len; ++ i)
+        for (int j = 0; j < len; ++ j)
+            test[i + j] += ans[i] * ans[j];
+    for (auto &ii : test) ii %= 10;
+    int cur = 0; 
+    bool ret = true;
+    while (s[cur]) ret &= (s[cur] == test[cur] + '0'), ++ cur;
+    if (ret)
+    {
+        reverse(ans, ans + len);
+        longs aa = 0;
+        for (int i = 0; i < len; ++ i)
+            aa *= 10, aa += ans[i];
+        minimize(a, aa);    
+    }
+    return ret;     
+}
+
+bool dfs(int now, int lim)
+{
+    if (now >= lim) return check(lim);
+    int C = 0;
+    for (int i = 1; i < now; ++ i)
+        C = (C + ans[i] * ans[now - i]) % 10;
+    int need = (s[now] - '0' + 10 - C) % 10;
+    bool _found = false;
+    for (int i = 0; i < 10; ++ i)
+        if (mul[2][i] == need)
+        {
+            bool _tmp = false;
+            for (int j = 0; j < 10; ++ j)
+                if (mul[ans[0]][j] == i)
+                    ans[now] = j, _tmp |= dfs(now + 1, lim);
+            _found |= _tmp;        
+        }
+    return _found;    
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+#if 0
+    freopen("in.txt", "r", stdin);
+#endif
+    for (int i = 0; i < 10; ++ i)
+        for (int j = 0; j < 10; ++ j)
+            mul[i][j] = (i * j) % 10;
+    for (int i = 0; i < 10; ++ i)
+        prod[mul[i][i]][i] = true;
+    scanner(s);
+    int N = strlen(s);
+    reverse(s, s + N);
+    if (0 == N % 2) return println("-1"), 0;
+    int len = (N + 1) / 2;
+    bool found = false;
+    for (int i = 0; i < 10; ++ i)
+        if (prod[s[0] - '0'][i])
+            ans[0] = i, found |= dfs(1, len);
+    if (found) println(a);
+    else puts("-1");      
+    return 0;
+}
