72a4fa15742453421a2f0c9ade3553701c6e920c
shiroha <ymd45921@163.com>
2021-03-23 22:17:38 +0800

    每日一套：就这？

---
+++ playground/2021-Spr/cf1484/C.cc
@@ -0,0 +1,148 @@
+/**
+ *
+ * 它竟然不是纸老虎
+ * 纸老虎竟然是我自己
+ * 
+ * 越来是我读题不长眼
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+using longs = long long;
+using uint = unsigned;
+using ulongs = unsigned long long;
+using longd = long double;
+using lll = __int128;
+
+#define minimize(a, b...) ((a) = min({(a), b}))
+#define maximize(a, b...) ((a) = max({(a), b}))
+#define sgn(x) ((x) < 0 ? -1 : (x) > 0)
+#define puti(n) puts(to_string(n).c_str())
+
+#if 1
+#define eprintf(x...) fprintf(stderr, x)
+#define eputs(str) fputs(str, stderr), fputc('\n', stderr)
+#define var(x) ""#x" = " << x
+#define watch(...) trace(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define eprintf(...)
+#define eputs(...)
+#define watch(...)
+#endif
+
+template <class printable>
+void trace(const char *name, printable &&value)
+{cerr << name << " = " << value << endl;}
+template <class printable, class ...args>
+void trace(const char *names, printable &&value, args &&...list)
+{
+    const char *separate = strchr(names + 1, ',');
+    cerr.write(names, separate - names) << " = " << value << ',';
+    trace(separate + 1, list...);
+}
+
+template <class T>
+void print(T x)
+{
+    if (x < 0) { putchar('-'); x = -x; }
+    static char str[40]; int cnt = 0;
+    while (x > 9) { str[cnt ++] = (x % 10) + 48; x /= 10;}
+    str[cnt ++] = x + 48;
+    while (cnt --) putchar(str[cnt]);
+}
+void print(const char *s) {fputs(s, stdout);}
+void print(char *s) {fputs(s, stdout);}
+void print(string &s) {print(s.c_str());}
+void print(char ch) {putchar(ch);}
+template <class T, class ...Ts>
+void print(T x, Ts ...xs) {print(x), print(xs...);}
+void println() {puts("");}
+template <class T>
+void println(T x)
+{print(x), putchar('\n');}
+
+static class Scanner
+{
+    template<class T>
+    inline T read()
+    {
+        T x = 0; int f = 0, ch = getchar();
+        while (!isdigit(ch)) ch == '-' && (f = !f), ch = getchar();
+        while (isdigit(ch)) x = x * 10 + ch - 48, ch = getchar();
+        return f ? -x : x;
+    }
+
+    static inline bool isBlank(int x)
+    {return x == ' ' || x == '\n';}
+public:
+
+    template <class T>
+    void operator()(T &x){x = read<T>();}
+
+    template <class T, class... Ts>
+    void operator()(T &x, Ts &... y)
+    {(*this)(x), (*this)(y...);}
+    template <class T, class... Ts>
+    void operator()(T &&x, Ts &&... y)
+    {(*this)(x), (*this)(y...);}
+
+    void operator()(char *x){scanf("%s", x);}
+    void operator()(char &x){do x = (char)getchar(); while (isBlank(x));}
+
+    int nextInt() {return read<int>();}
+    longs nextLongs() {return read<longs>();}
+    lll nextInt128() {return read<lll>();}
+    char nextChar() {char x; (*this)(x); return x;}
+} scanner;
+
+struct fast_read
+{
+    template<class name>
+    fast_read operator, (name &x)
+    {scanner(x); return fast_read{};}
+} fastRead;
+#define $$ fastRead,
+
+const int N = 1e5 + 5;
+vector<int> f[N];
+int c[N], cnt[N];
+
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+#if 0
+    freopen("in.txt", "r", stdin);
+#endif
+    int T = scanner.nextInt();
+    while (T --)
+    {
+        int n = scanner.nextInt(),
+            m = scanner.nextInt();
+        int lim = (m + 1) / 2;
+        memset(cnt, 0, sizeof(int) * (n + 1));
+        memset(c, 0, sizeof(int) * (m + 1));
+        for (int i = 1; i <= m; ++ i)
+        {
+            f[i].clear();
+            int k = scanner.nextInt();
+            while (k --)
+                f[i].push_back(scanner.nextInt());
+        }
+        for (int i = 1; i <= m; ++ i)
+            if (f[i].size() == 1)
+                ++ cnt[f[i][0]], c[i] = f[i][0];
+        int xx = *max_element(cnt + 1, cnt + 1 + n);
+        if (xx > lim) { println("NO"); continue; }
+        for (int i = 1; i <= m; ++ i)
+            if (!c[i]) for (auto ii : f[i])
+                if (cnt[ii] < lim)
+                {c[i] = ii, ++ cnt[ii]; break;}
+        println("YES");
+        for (int i = 1; i <= m; ++ i)
+            print(c[i], " \n"[i == m]);
+    }
+    return 0;
+}
