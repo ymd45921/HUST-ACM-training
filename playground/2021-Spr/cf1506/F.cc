be281176edb3f83b1c651a1224aef8f2ca2dfc83
shiroha <ymd45921@163.com>
2021-03-26 00:38:31 +0800

    每日一套：差点 AK 这傻逼 div3

---
+++ playground/2021-Spr/cf1506/F.cc
@@ -0,0 +1,154 @@
+/**
+ *
+ *
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+using longs = long long;
+using uint = unsigned;
+using ulongs = unsigned long long;
+using longd = long double;
+using lll = __int128;
+
+#define minimize(a, b...) ((a) = min({(a), b}))
+#define maximize(a, b...) ((a) = max({(a), b}))
+#define sgn(x) ((x) < 0 ? -1 : (x) > 0)
+#define puti(n) puts(to_string(n).c_str())
+
+#if 1
+#define eprintf(x...) fprintf(stderr, x)
+#define eputs(str) fputs(str, stderr), fputc('\n', stderr)
+#define var(x) ""#x" = " << x
+#define watch(...) trace(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define eprintf(...)
+#define eputs(...)
+#define watch(...)
+#endif
+
+template <class printable>
+void trace(const char *name, printable &&value)
+{cerr << name << " = " << value << endl;}
+template <class printable, class ...args>
+void trace(const char *names, printable &&value, args &&...list)
+{
+    const char *separate = strchr(names + 1, ',');
+    cerr.write(names, separate - names) << " = " << value << ',';
+    trace(separate + 1, list...);
+}
+
+template <class T>
+void print(T x)
+{
+    if (x < 0) { putchar('-'); x = -x; }
+    static char str[40]; int cnt = 0;
+    while (x > 9) { str[cnt ++] = (x % 10) + 48; x /= 10;}
+    str[cnt ++] = x + 48;
+    while (cnt --) putchar(str[cnt]);
+}
+void print(const char *s) {fputs(s, stdout);}
+void print(char *s) {fputs(s, stdout);}
+void print(string &s) {print(s.c_str());}
+void print(char ch) {putchar(ch);}
+template <class T, class ...Ts>
+void print(T x, Ts ...xs) {print(x), print(xs...);}
+void println() {puts("");}
+template <class T>
+void println(T x)
+{print(x), putchar('\n');}
+
+static class Scanner
+{
+    template<class T>
+    inline T read()
+    {
+        T x = 0; int f = 0, ch = getchar();
+        while (!isdigit(ch)) ch == '-' && (f = !f), ch = getchar();
+        while (isdigit(ch)) x = x * 10 + ch - 48, ch = getchar();
+        return f ? -x : x;
+    }
+
+    static inline bool isBlank(int x)
+    {return x == ' ' || x == '\n';}
+public:
+
+    template <class T>
+    void operator()(T &x){x = read<T>();}
+
+    template <class T, class... Ts>
+    void operator()(T &x, Ts &... y)
+    {(*this)(x), (*this)(y...);}
+    template <class T, class... Ts>
+    void operator()(T &&x, Ts &&... y)
+    {(*this)(x), (*this)(y...);}
+
+    void operator()(char *x){scanf("%s", x);}
+    void operator()(char &x){do x = (char)getchar(); while (isBlank(x));}
+
+    int nextInt() {return read<int>();}
+    longs nextLongs() {return read<longs>();}
+    lll nextInt128() {return read<lll>();}
+    char nextChar() {char x; (*this)(x); return x;}
+} scanner;
+
+struct fast_read
+{
+    template<class name>
+    fast_read operator, (name &x)
+    {scanner(x); return fast_read{};}
+} fastRead;
+#define $$ fastRead,
+
+const int N = 2e5 + 5;
+using point = pair<int, int>;
+point a[N];
+
+int getLayer(const point &p)
+{return p.first - p.second;}
+
+int getId(const point &p)
+{return p.first + p.second;}
+
+bool compare(const point &lhs, const point &rhs)
+{
+    int ll = getLayer(lhs), rl = getLayer(rhs);
+    if (ll != rl) return ll < rl;
+    else return getId(lhs) < getId(rhs);
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+#if 0
+    freopen("in.txt", "r", stdin);
+#endif
+    int T = scanner.nextInt();
+    while (T --)
+    {
+        int n = scanner.nextInt();
+        for (int i = 1; i <= n; ++ i)
+            a[i].first = scanner.nextInt();
+        for (int i = 1; i <= n; ++ i)
+            a[i].second = scanner.nextInt();
+        a[0] = {1, 1};
+        sort(a + 1, a + 1 + n, compare);
+        longs ans = 0;
+        for (int i = 1; i <= n; ++ i)
+        {
+            const auto &last = a[i - 1], &now = a[i];
+            int ll = getLayer(last), nl = getLayer(now);
+            if (ll == nl)
+                ans += !(ll % 2) * (getId(now) - getId(last)) / 2;
+            else
+            {
+                int d = nl - ll;
+                ans += d / 2 + (d % 2) * (ll % 2);
+            }
+        }
+        println(ans);
+    }
+    return 0;
+}
