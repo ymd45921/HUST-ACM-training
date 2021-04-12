fd71e7712d77108b97b17b0675a488313c77ee59
shiroha <ymd45921@163.com>
2021-04-13 00:37:05 +0800

    每日一套：名为 E 的诅咒（

---
+++ playground/2021-Spr/cf1511/C.cc
@@ -0,0 +1,398 @@
+/**
+ *
+ *
+ */
+#include <bits/stdc++.h>
+
+#define minimize(a, b...) ((a) = min({(a), b}))
+#define maximize(a, b...) ((a) = max({(a), b}))
+#define sgn(x) ((x) < 0 ? -1 : (x) > 0)
+#define watch(...) trace(#__VA_ARGS__, __VA_ARGS__)
+#define eprintf(x...) fprintf(stderr, x)
+#define var(x) ""#x" = " << x
+#define $$(...) $.trace(#__VA_ARGS__, __VA_ARGS__)
+
+static class cquery {
+
+    static constexpr char _blank[] = " \n";
+    static constexpr char _break[] = "\n";
+    static constexpr size_t buffer_size = 100;
+    static char buffer[buffer_size];
+
+    template<class T>
+    inline T read() {
+        T ret = 0;
+        int f = 0, ch = ::getchar();
+        for (; !isdigit(ch); ch = ::getchar())
+            if (ch == '-') f = !f;
+        for (; isdigit(ch); ch = ::getchar())
+            ret = ret * 10 + ch - 48;
+        return f ? -ret : ret;
+    }
+
+    template<class T>
+    static void write(FILE *io, T x) {
+        if (x < 0) {
+            fputc('-', io);
+            x = -x;
+        }
+        static char *str = buffer;
+        int cnt = 0;
+        while (x > 9) {
+            str[cnt++] = (x % 10) + 48;
+            x /= 10;
+        }
+        str[cnt++] = x + 48;
+        while (cnt--) fputc(str[cnt], io);
+    }
+
+    static void write(FILE *io, const double x) {
+        sprintf(buffer, "%lf", x), write(io, buffer);
+    }
+
+    static void write(FILE *io, const long double x) {
+        sprintf(buffer, "%Lf", x), write(io, buffer);
+    }
+
+    static void write(FILE *io, const float x) {
+        sprintf(buffer, "%f", x), write(io, buffer);
+    }
+
+    static void write(FILE *io, const char *const x) { fputs(x, io); }
+
+    static void write(FILE *io, char *const x) { fputs(x, io); }
+
+    static void write(FILE *io, const std::string &x) { write(io, x.c_str()); }
+
+    static void write(FILE *io, const char x) { fputc(x, io); }
+
+    template<const char *charset>
+    static inline bool is(int x) {
+        if (x <= 0) return true;
+        const char *syb = charset;
+        for (; *syb; ++syb) if (*syb == x) return true;
+        return false;
+    }
+
+    template<class T, class ...Ts>
+    void write(FILE *io, const T &x, Ts ...xs) {
+        write(io, x), write(io, xs...);
+    }
+
+    template<class T, class ...Ts>
+    void write(FILE *io, const T &&x, Ts ...xs) {
+        write(io, x), write(io, xs...);
+    }
+
+    static char *readToken() {
+        char *cur = buffer;
+        int ch = ::getchar();
+        while (is<_blank>(ch)) ch = ::getchar();
+        for (; !is<_blank>(ch); ch = ::getchar())
+            *cur++ = (char) ch;
+        return *cur = '\0', cur;
+    }
+
+    static void flush(FILE *stream) { fflush(stream); }
+
+    static int eof(FILE *stream) { return feof(stream); }
+
+public:
+
+    template<class T>
+    cquery &operator()(T &x) {
+        x = read<T>();
+        return *this;
+    }
+
+    template<class T, class... Ts>
+    cquery &operator()(T &x, Ts &... y) {
+        return (*this)(x), (*this)(y...);
+    }
+
+    template<class... Ts>
+    cquery &operator()(char *x, Ts &&... y) {
+        return (*this)(x), (*this)(y...);
+    }
+
+    cquery &operator()(char *x) {
+        scanf("%s", x);
+        return *this;
+    }
+
+    cquery &operator()(double &x) {
+        auto end = readToken();
+        x = strtod(buffer, &end);
+        return *this;
+    }
+
+    cquery &operator()(long double &x) {
+        auto end = readToken();
+        x = strtold(buffer, &end);
+        return *this;
+    }
+
+    cquery &operator()(float &x) {
+        auto end = readToken();
+        x = strtof(buffer, &end);
+        return *this;
+    }
+
+    template <class T1, class T2>
+    cquery &operator()(std::pair<T1, T2> &x) {
+        return (*this)(x.first, x.second);
+    }
+
+    cquery &operator()(char &x) {
+        do x = (char) ::getchar(); while (is<_blank>(x));
+        return *this;
+    }
+
+private:
+
+    template<class T>
+    T next() {
+        T ret;
+        (*this)(ret);
+        return ret;
+    }
+
+public:
+
+    int nextInt() { return read<int>(); }
+
+    auto nextLong() { return read<long long>(); }
+
+    auto next128() { return read<__int128>(); }
+
+    char getchar() { return next<char>(); }
+
+    double nextDouble() { return next<double>(); }
+
+    auto nextFloat() { return next<float>(); }
+
+    cquery &flush() { return flush(stdout), *this; }
+
+    bool hasNext() { return this, !eof(stdin); }
+
+    template<class T>
+    cquery &print(const T &x) { return write(stdout, x), *this; }
+
+    template<class T>
+    cquery &print(const T &&x) { return write(stdout, x), *this; }
+
+    template<class T, class ...Ts>
+    cquery &print(T &x, Ts ...xs) {
+        return write(stdout, x, xs...), *this;
+    }
+
+    template<class T, class ...Ts>
+    cquery &print(T &&x, Ts ...xs) {
+        return write(stdout, x, xs...), *this;
+    }
+
+    template<class T>
+    cquery &println(const T &x) {
+        return write(stdout, x, '\n'), *this;
+    }
+
+    template<class T>
+    cquery &println(const T &&x) {
+        return write(stdout, x, '\n'), *this;
+    }
+
+    template<class T, class ...Ts>
+    cquery &println(const T &x, Ts ...xs) {
+        return write(stdout, x, xs..., '\n'), *this;
+    }
+
+    template<class T, class ...Ts>
+    cquery &println(const T &&x, Ts ...xs) {
+        return write(stdout, x, xs..., '\n'), *this;
+    }
+
+    template<class T>
+    cquery &put(const T &x) {
+        return write(stdout, x, '\n'), *this;
+    }
+
+    template<class T>
+    cquery &put(const T &&x) {
+        return write(stdout, x, '\n'), *this;
+    }
+
+    template<class T, class ...Ts>
+    cquery &put(const T &x, Ts ...xs) {
+        write(stdout, x, ' '), put(xs...);
+        return *this;
+    }
+
+    template<class T, class ...Ts>
+    cquery &put(const T &&x, Ts ...xs) {
+        write(stdout, x, ' '), put(xs...);
+        return *this;
+    }
+
+    template<class T>
+    cquery &nextArray(T first, T last) {
+        while (first != last)
+            (*this)(*first), ++first;
+        return *this;
+    }
+
+    template<class T, class Fun = void(*)(T, cquery &)>
+    cquery &nextArray(T first, T last, Fun lambda) {
+        while (first != last)
+            lambda(first, *this), ++first;
+        return *this;
+    }
+
+    template<class T>
+    cquery &putArray(T first, T last, char split = ' ') {
+        while (first != last) {
+            write(stdout, *first), ++first;
+            if (first != last) write(stdout, split);
+            else write(stdout, '\n');
+        }
+        return *this;
+    }
+
+    template<class T>
+    cquery &putArray(T first, T last, char *split) {
+        while (first != last) {
+            write(stdout, *first), ++first;
+            if (first != last) write(stdout, split);
+            else write(stdout, '\n');
+        }
+        return *this;
+    }
+
+    template<class T, class Fun = void(*)(T, cquery &), class comma = char>
+    cquery &putArray(T first, T last, Fun fmt, comma split = ' ') {
+        while (first != last) {
+            fmt(first, *this), ++first;
+            if (first != last) write(stdout, split);
+            else write(stdout, '\n');
+        }
+        return *this;
+    }
+
+    template<class T>
+    cquery &log(const T &x) {
+        return write(stdout, x, '\n'), *this;
+    }
+
+    template<class T>
+    cquery &log(const T &&x) {
+        return write(stderr, x, '\n'), *this;
+    }
+
+    template<class T, class ...Ts>
+    cquery &log(const T &x, Ts ...xs) {
+        write(stderr, x, ' '), put(xs...);
+        return *this;
+    }
+
+    template<class T, class ...Ts>
+    cquery &log(const T &&x, Ts ...xs) {
+        write(stderr, x, ' '), put(xs...);
+        return *this;
+    }
+
+    template<class T>
+    cquery &logArray(T first, T last, char split = ' ') {
+        while (first != last) {
+            write(stderr, *first), ++first;
+            if (first != last) write(stderr, split);
+            else write(stderr, '\n');
+        }
+        return *this;
+    }
+
+    template<class T>
+    cquery &logArray(T first, T last, char *split) {
+        while (first != last) {
+            write(stderr, *first), ++first;
+            if (first != last) write(stderr, split);
+            else write(stderr, '\n');
+        }
+        return *this;
+    }
+
+    template<class T, class Fun = void(*)(T, cquery &), class comma = char>
+    cquery &logArray(T first, T last, Fun fmt, comma split = ' ') {
+        while (first != last) {
+            fmt(first, *this), ++first;
+            if (first != last) write(stderr, split);
+            else write(stderr, '\n');
+        }
+        return *this;
+    }
+
+    cquery &newLine() { return write(stdout, '\n'), *this; }
+
+    [[deprecated]] cquery &println() { return newLine(); }
+
+    [[deprecated]] cquery &put() { return newLine(); }
+
+    template<class T>
+    void trace(const char *name, T &&value) { write(stderr, name, " = ", value); }
+
+    template<class T, class ...Ts>
+    void trace(const char *names, T &&value, Ts &&...list) {
+        const char *separate = strchr(names + 1, ',');
+        while (names != separate) fputc(*names++, stderr);
+        write(stderr, " = ", value), fputc(',', stderr);
+        trace(separate + 1, list...);
+    }
+} $;
+
+char cquery::buffer[cquery::buffer_size];
+
+struct {
+    template <class token>
+    auto &operator,(token &x) { return $(x), *this; }
+    auto &operator,(char *x) { return $(x), *this; }
+} input;
+
+struct {
+    using linebreak = std::ostream &(*)(std::ostream &);
+    auto &operator,(linebreak x) { return $.newLine().flush(), *this; }
+    template <class token>
+    auto &operator,(token &x) { return $.print(x), *this; }
+    template <class token>
+    auto &operator,(token &&x) { return $.print(x), *this; }
+} output;
+
+using namespace std;
+
+using longs = long long;
+using uint = unsigned;
+using lll = __int128;
+
+const auto null = nullptr;
+
+const int N = 3e5 + 5;
+int a[N], n, q, top[100], t;
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(null), cout.tie(null);
+
+    $(n, q).nextArray(a + 1, a + 1 + n);
+    for (int i = 1; i <= n; ++ i) {
+        if (!top[a[i]]) top[a[i]] = i;
+    }
+    vector<int> ans;
+    while (q --) {
+        t = $.nextInt();
+        ans.push_back(top[t]);
+        for (int i = 1; i <= 50; ++ i)
+            if (top[i] && i != t && top[i] < top[t])
+                ++ top[i];
+        top[t] = 1;
+    }
+    $.putArray(ans.begin(), ans.end());
+
+    return 0;
+}
