d8af5a53df05e798dbcb6ad093b30c41a6e2bf20
shiraha <ymd45921@163.com>
2021-06-06 22:37:41 +0800

    经典的赛后 A 题：不过这不是我 A 的，是阿潘（）我连高中数学的数列通项都不会求了==

---
+++ official/2021 CCPC Xiangtan Invitation/E.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using llong = long long;
+
+const llong mod = 1e9 + 7;
+
+#define bigint int
+bigint read() {
+    bigint ret = 0;
+    int f = 0, ch = getchar();
+    for (; !isdigit(ch); ch = getchar())
+        if (ch == '-') f = !f;
+    for (; isdigit(ch); ch = getchar())
+        ret = ret * 10 + ch - 48;
+    return f ? -ret : ret;
+}
+#undef bigint
+
+llong fastPow(llong a, llong b) {
+    llong ans = 1;
+    while (b) {
+        if (b & 1u) ans = (ans * a) % mod;
+        a = (a * a) % mod;
+        b >>= 1u;
+    }
+    return ans % mod;
+}
+
+int mem[] = {0, 0, 0, 0, 1, 4, 12, 31};
+
+signed main() {
+    for (int T = read(); T --;) {
+        int n = read();
+        if (n <= 7) {
+            cout << mem[n] << endl;
+            continue;
+        }
+        llong ans = fastPow(2, n - 4) * (n - 3) % mod;
+        llong tmp = fastPow(2, n - 7) * (mod + 1 - fastPow(mod - 2, (n - 1) / 3 - 1)) % mod;
+        llong ano = fastPow(8, mod - 2) * fastPow(2, n - (n - 1) / 3 * 3 - 1) % mod * (n - (n - 1) / 3 * 3) % mod * fastPow(mod - 1, (n - 1) / 3 + 1) % mod;
+        cout << (ans + tmp + ano) % mod << endl;
+    }
+    return 0;
+}
